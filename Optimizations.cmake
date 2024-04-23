987ddc7e903aa9882cd55f6d0fbd823cc320beb7
floyd <floyd.zweydinger@rub.de>
2024-04-23 19:46:43 +0200

    gitbak

---
+++ Optimizations.cmake
@@ -0,0 +1,19 @@
+get_filename_component(_currentDir "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+# if this flag is set, the cache sizes will not added to the compile arguments
+SET(CMAKE_CACHE_DO_NOT_ADD_TO_FLAGS 0)
+
+# it this flag is set, the host compiler optimizations/vectorization flags are
+# not added to the compile arguments
+SET(CMAKE_HOST_DO_NOT_ADD_TO_FLAGS 0)
+
+SET(WRITE_CONFIG_FILE 0)
+SET(CONFIG_FILE "config.h")
+
+# NOTE: the order is important
+include(${CMAKE_CURRENT_LIST_DIR}/architecture.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/compiler.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/cache.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/CompilerOptimizations.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/HostOptimizations.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/autovectorize/Autovectorize.cmake)
