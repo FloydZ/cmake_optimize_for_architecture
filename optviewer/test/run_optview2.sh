3fbdd5d2a51aa8ebb7427f19764f3f26057e7130
floyd <floyd.zweydinger@rub.de>
2025-03-24 22:33:54 +0100

    better optview

---
+++ optviewer/test/run_optview2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")" || exit 1
+
+echo "Running make..."
+make
+
+echo "Running optview2..."
+python3 ../opt-viewer.py --open-browser --output-dir ./out --source-dir ./ ./out
